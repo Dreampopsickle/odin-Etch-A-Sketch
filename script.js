e6909d2a80e5b5c0f346e8350fe4415df844286e
Christian Rakotoarisoa <CRakotoarisoa@gmail.com>
2024-10-01 14:49:35 -0400

    finish etch a sketch

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let container = document.querySelector("#container");
-const createSquares = () => {
-  for (let i = 1; i <= 16; i++) {
-    let element = document.createElement("div"); // creates the setup for a div
-    element.className = "box";
-    element.innerHTML = `${i}`;
-    container.appendChild(element); // applies setup to html
+const container = document.querySelector(".container");
+const resetButton = document.querySelector("#resetGrid");
+
+const createGrid = (squaresPerSide) => {
+  container.innerHTML = "";
+
+  const containerSize = container.clientWidth;
+  const squareSize = containerSize / squaresPerSide;
+
+  for (let i = 1; i < squaresPerSide * squaresPerSide; i++) {
+    const div = document.createElement("div");
+    div.id = "grid-item";
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
+
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+
+    container.appendChild(div);
   }
 
-  console.log(container);
+  container.style.gap = `${squareSize * 0.02}px`;
 };
 
-const createGrid = () => {
-  for (let j = 1; j <= 16; j++) {
-    createSquares();
+const resetGrid = () => {
+  let newSize = prompt("Enter the number of squares per side (max 100):", 16);
+  if (newSize < 1 || newSize > 100 || isNaN(newSize)) {
+    alert("Invalid Input. Please enter a nunber between 1 and 100.");
+  } else {
+    createGrid(newSize);
   }
 };
 
-createGrid();
+createGrid(16);
+
+resetButton.addEventListener("click", resetGrid);
